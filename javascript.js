c8daeeda649ae83a2665c930c0a36240b1f563d8
Styles <139181994+stylescode@users.noreply.github.com>
2023-09-27 16:18:03 -0700

    Add js and css to check pswds match

--- javascript.js
+++ javascript.js
@@ -1,5 +1,25 @@
+let pass1 = document.querySelector('#pass1');
+let pass2 = document.querySelector('#pass2');
+let result = document.querySelector('#pswdmatch');
 
 
 // check password/confirm password are the same
-function checkPassword {
+function checkPassword() {
+  if (pass1.value !== pass2.value) {
+    result.textContent = '*Passwords do not match';
+    pass1.classList.add('redborder');
+    pass2.classList.add('redborder');
+  } else {
+    result.textContent = '';
+    pass1.classList.remove('redborder');
+    pass2.classList.remove('redborder');
+  }
 }
+
+pass1.addEventListener('keyup', () => {
+  if (pass2.value !== '') {
+    checkPassword();
+  }
+});
+
+pass2.addEventListener('keyup', checkPassword);
